48e406520bbd5782d29f058e1486ab22cd601bc4
josueluna315 <josue.luna@ciencias.unam.mx>
2026-06-03 16:46:34 -0600

    Update conexionMySQL.java

--- src/conexion/conexionMySQL.java
+++ src/conexion/conexionMySQL.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexionMySQL {
+public class conexionMySQL {
     private static final String URL    = "jdbc:mysql://localhost:3306/torneo_ajedrez";
     private static final String USUARIO = "root";
     private static final String CLAVE   = "tu_contraseña";
