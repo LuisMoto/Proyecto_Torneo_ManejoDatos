5ed50b3d519333749f6994545d14133969c960fd
josueluna315 <josue.luna@ciencias.unam.mx>
2026-06-07 13:57:47 -0600

    Add documentation for JugadorClonException

--- src/excepciones/JugadorClonException.java
+++ src/excepciones/JugadorClonException.java
@@ -1,9 +1,20 @@
 package excepciones;
 
+/**
+ * Excepción que se lanza cuando se intenta programar
+ * una partida utilizando el mismo jugador para las
+ * piezas blancas y negras.
+ */
 public class JugadorClonException extends TorneoAjedrezException {
 
+    /**
+     * Construye una excepción indicando los ID
+     * de los jugadores seleccionados.
+     *
+     * @param idBlancas ID del jugador asignado a las blancas.
+     * @param idNegras ID del jugador asignado a las negras.
+     */
     public JugadorClonException(int idBlancas,int idNegras) {
-
         super("El jugador de blancas (" + idBlancas + ") y el jugador de negras (" + idNegras + ") no pueden ser el mismo.", "JUGADOR_CLON");
     }
 }
