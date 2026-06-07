5b32ea1179cb1d5b441264c9e046db92ce38064b
josueluna315 <josue.luna@ciencias.unam.mx>
2026-06-07 13:57:12 -0600

    Update FechaInvalidaException.java

--- src/excepciones/FechaInvalidaException.java
+++ src/excepciones/FechaInvalidaException.java
@@ -1,8 +1,15 @@
 package excepciones;
 
-public class FechaInvalidaException
-        extends TorneoAjedrezException {
+/**
+ * Excepción que se lanza cuando una fecha ingresada
+ * no cumple con el formato esperado o no es válida.
+ */
+public class FechaInvalidaException extends TorneoAjedrezException {
 
+     /**
+     * Construye una excepción para indicar
+     * que la fecha proporcionada es inválida.
+     */        
     public FechaInvalidaException() {
         super("La fecha ingresada no es válida.","FECHA_INVALIDA");
     }
